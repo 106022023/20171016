c3b878a811fc1cbad021bdd0a3c4f4b4feb1c5a5
I432 <evan032911@gmail.com>
2017-10-16 15:22:02 +0800

    1016

---
+++ src/cylinder.java
@@ -0,0 +1,12 @@
+public class cylinder {
+    public static void main(String[] args){
+        //顯示訊息
+        java.util.Scanner sc =new java.util.Scanner(System.in);
+
+        float r=sc.nextFloat();
+        float h=sc.nextFloat();
+
+        System.out.println(Math.pow(r,2)*h);
+    }
+}
+
